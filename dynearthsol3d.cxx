7fc12acb560af80048601fdd8fabc1d132a9903a
Eh Tan <tan2@mail.utexas.edu>
2012-09-12 13:19:20 +0800

    moved getting input parameters to a new function; parameters have nested scope as the input file.

--- dynearthsol3d.cxx
+++ dynearthsol3d.cxx
@@ -7,12 +7,68 @@ namespace po = boost::program_options;
 //#include <string>
 
 
-struct param {
+struct Sim {
     int max_steps;
     double max_time;
+};
 
+
+struct Mesh {
     double xlength, ylength, zlength;
 };
+
+struct Param {
+    Sim sim;
+    Mesh mesh;
+};
+
+
+void get_input_parameters(const char* filename, Param& param)
+{
+    std::ifstream ifs(filename);
+    if (!ifs) {
+        std::cerr << "Error: cannot open config_file '" << filename << "'\n";
+        std::exit(1);
+    }
+
+    //
+    // declare input parameters
+    //
+    po::options_description cfg("Config file options");
+    cfg.add_options()
+        ("sim.max_steps", po::value<int>(&param.sim.max_steps), "Max. number of time steps")
+        ("sim.max_time", po::value<double>(&param.sim.max_time), "Max. time (in seconds)")
+        ;
+
+    cfg.add_options()
+        ("mesh.xlength", po::value<double>(&param.mesh.xlength)->required(), "Length of x (in meters)")
+        ("mesh.ylength", po::value<double>(&param.mesh.ylength)->required(), "Length of y (in meters)")
+        ("mesh.zlength", po::value<double>(&param.mesh.zlength)->required(), "Length of z (in meters)")
+        ;
+
+    //
+    // parse config file
+    //
+    po::variables_map vm;
+    try {
+        po::store(po::parse_config_file(ifs, cfg), vm);
+        po::notify(vm);
+    }
+    catch (std::exception& e) {
+        std::cerr << "Error reading config_file '" << filename << "'\n";
+        std::cerr << e.what() << "\n";
+        std::exit(1);
+    }
+
+    //
+    // validate parameters
+    //
+    if ( !(vm.count("sim.max_steps") || vm.count("sim.max_time")) ) {
+        std::cerr << "Must provide either sim.max_steps or sim.max_time\n";
+        std::exit(1);
+    }
+}
+
 
 
 int main(int argc, const char *argv[])
@@ -25,49 +81,8 @@ int main(int argc, const char *argv[])
         return -1;
     }
 
-    std::ifstream ifs(argv[1]);
-    if (!ifs) {
-        std::cerr << "Error: cannot open config_file '" << argv[1] << "'\n";
-        return 1;
-    }
-
-    //
-    // declare input parameters
-    //
-    struct param param;
-    po::options_description cfg("Config file options");
-    cfg.add_options()
-        ("sim.max_steps", po::value<int>(&param.max_steps), "Max. number of time steps")
-        ("sim.max_time", po::value<double>(&param.max_time), "Max. time (in seconds)")
-        ;
-
-    cfg.add_options()
-        ("mesh.xlength", po::value<double>(&param.xlength)->required(), "Length of x (in meters)")
-        ("mesh.ylength", po::value<double>(&param.ylength)->required(), "Length of y (in meters)")
-        ("mesh.zlength", po::value<double>(&param.zlength)->required(), "Length of z (in meters)")
-        ;
-
-    //
-    // parse config file
-    //
-    po::variables_map vm;
-    try {
-        po::store(po::parse_config_file(ifs, cfg), vm);
-        po::notify(vm);
-    }
-    catch (std::exception& e) {
-        std::cerr << "Error reading config_file '" << argv[1] << "'\n";
-        std::cerr << e.what() << "\n";
-        return 1;
-    }
-
-    //
-    // validate parameters
-    //
-    if ( !(vm.count("sim.max_steps") || vm.count("sim.max_time")) ) {
-        std::cerr << "Must provide either sim.max_steps or sim.max_time\n";
-        return 1;
-    }
+    Param param;
+    get_input_parameters(argv[1], param);
 
     //
     // run simulation
@@ -79,7 +94,7 @@ int main(int argc, const char *argv[])
         std::cout << "Step: " << steps << ", time:" << time << "\n";
         steps++;
         time += dt;
-    } while (steps <= param.max_steps && time <= param.max_time);
+    } while (steps <= param.sim.max_steps && time <= param.sim.max_time);
 
 
     //
