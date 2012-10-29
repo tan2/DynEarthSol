f3bd22c596b1a65b6ca764b37f6f78be31ff1049
Eh Tan <tan2@mail.utexas.edu>
2012-10-29 09:52:44 +0800

    using shift operator to define binary flag

--- constants.hpp
+++ constants.hpp
@@ -18,12 +18,12 @@ const int NODES_PER_ELEM = NDIMS + 1;
 const int NSTR = NDIMS * (NDIMS + 1) / 2;
 
 // Flags for boundary
-const int BOUNDX0 = 0x00000001;
-const int BOUNDX1 = 0x00000010;
-const int BOUNDY0 = 0x00000100;
-const int BOUNDY1 = 0x00001000;
-const int BOUNDZ0 = 0x00010000;
-const int BOUNDZ1 = 0x00100000;
+const int BOUNDX0 = 1 << 0;
+const int BOUNDX1 = 1 << 1;
+const int BOUNDY0 = 1 << 2;
+const int BOUNDY1 = 1 << 3;
+const int BOUNDZ0 = 1 << 4;
+const int BOUNDZ1 = 1 << 5;
 
 
 const double YEAR2SEC = 365.2422 * 86400;
