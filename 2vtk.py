816650d7aa660f8f39f280218b23e6b498d7a176
Eh Tan <tan2@mail.utexas.edu>
2012-11-06 15:34:36 +0800

    more data to vtk files, more options

--- 2vtk.py
+++ 2vtk.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
 '''Convert the binary output of DynEarthSol3D to VTK files.
 
-usage: 2vtk.py [-2 or -3] modelname [start [end]]
+usage: 2vtk.py [-2 -3 -a -t -h] modelname [start [end]]
 
-Using -2 (default) or -3 to specify 2D or 3D data.
+options:
+    -2          save 2D data (default)
+    -3          save 3D data
+    -a          save data in ASCII format (default: binary)
+    -t          save all tensor components (default: no component)
+    -h,--help   show this help
 '''
 
 import sys, os
 import base64, zlib
 import numpy as np
 
+# 2D or 3D data?
+ndims = 2
+
 # Save in ASCII or encoded binary.
 # Some old VTK programs cannot read binary VTK files.
-output_in_binary = 0
+output_in_binary = True
+
+# Save indivisual components?
+output_tensor_components = False
 
 
-def main(ndims, modelname, start, end):
+def main(modelname, start, end):
     path = os.path.dirname(modelname)
     prefix = os.path.basename(modelname)
     if path:
@@ -29,6 +40,13 @@ def main(ndims, modelname, start, end):
 
     if end == -1:
         end = len(frames)
+
+    if ndims == 2:
+        nstr = 3
+        component = ('XX', 'ZZ', 'XZ')
+    else:
+        nstr = 6
+        component = ('XX', 'YY', 'ZZ', 'XY', 'XZ', 'YZ')
 
     for i, rec in enumerate(frames[start:end]):
         # convert from numpy.int to python int
@@ -47,7 +65,17 @@ def main(ndims, modelname, start, end):
         #
         fvtu.write('  <PointData>\n')
 
-        # temperature
+        vel = np.fromfile(prefix+'.vel.'+suffix, dtype=np.float64, count=ndims*nnode)
+        vel.shape = (nnode, ndims)
+        if ndims == 2:
+            # VTK requires vector field (velocity, coordinate) has 3 components.
+            # Allocating a 3-vector tmp array for VTK data output.
+            tmp = np.zeros((nnode, 3), dtype=vel.dtype)
+            tmp[:,:ndims] = vel
+        else:
+            tmp = vel
+        vtk_dataarray(fvtu, tmp, 'velocity', 3)
+
         temperature = np.fromfile(prefix+'.temperature.'+suffix, dtype=np.float64, count=nnode)
         vtk_dataarray(fvtu, temperature, 'temperature')
 
@@ -60,6 +88,40 @@ def main(ndims, modelname, start, end):
         # element-based field
         #
         fvtu.write('  <CellData>\n')
+
+        plstrain = np.fromfile(prefix+'.plstrain.'+suffix, dtype=np.float64, count=nelem)
+        vtk_dataarray(fvtu, plstrain, 'plastic strain')
+
+        strain_rate = np.fromfile(prefix+'.strain-rate.'+suffix, dtype=np.float64, count=nstr*nelem)
+        strain_rate.shape = (nelem, nstr)
+        srII = second_invariant(strain_rate)
+        vtk_dataarray(fvtu, np.log10(srII+1e-45), 'strain-rate II log10')
+        if output_tensor_components:
+            for d in range(nstr):
+                vtk_dataarray(fvtu, strain_rate[:,d], 'strain-rate ' + component[d])
+
+        strain = np.fromfile(prefix+'.strain.'+suffix, dtype=np.float64, count=nstr*nelem)
+        strain.shape = (nelem, nstr)
+        sI = first_invariant(strain)
+        sII = second_invariant(strain)
+        vtk_dataarray(fvtu, sI, 'strain I')
+        vtk_dataarray(fvtu, sII, 'strain II')
+        if output_tensor_components:
+            for d in range(nstr):
+                vtk_dataarray(fvtu, strain[:,d], 'strain ' + component[d])
+
+        stress = np.fromfile(prefix+'.stress.'+suffix, dtype=np.float64, count=nstr*nelem)
+        stress.shape = (nelem, nstr)
+        tI = first_invariant(stress)
+        tII = second_invariant(stress)
+        vtk_dataarray(fvtu, tI, 'stress I')
+        vtk_dataarray(fvtu, tII, 'stress II')
+        if output_tensor_components:
+            for d in range(nstr):
+                vtk_dataarray(fvtu, stress[:,d], 'stress ' + component[d])
+
+        effvisc = tII / (srII + 1e-45)
+        vtk_dataarray(fvtu, effvisc, 'effective viscosity')
 
         # element number for debugging
         vtk_dataarray(fvtu, np.arange(nelem, dtype=np.int32), 'elem#')
@@ -164,6 +226,23 @@ def vtu_footer(f):
     return
 
 
+def first_invariant(t):
+    return np.sum(t[:,:ndims], axis=1) / ndims
+
+
+def second_invariant(t):
+    '''The second invariant of a symmetric tensor t,
+    where t[:,0:ndims] are the diagonal components;
+      and t[:,ndims:] are the off-diagonal components.'''
+
+    if ndims == 2:
+        return 0.5 * np.sqrt((t[:,0] - t[:,1])**2 + 4 * t[:,2]**2)
+    else:
+        return np.sqrt(1./3 * (t[:,0] + t[:,1] + t[:,2])**2
+                       - (t[:,0]*t[:,1] + t[:,0]*t[:,2] + t[:,1]*t[:,2])
+                       + t[:,3]**2 + t[:,4]**2 + t[:,5]**2)
+
+
 if __name__ == '__main__':
 
     if len(sys.argv) < 2:
@@ -175,11 +254,22 @@ if __name__ == '__main__':
                 print __doc__
                 sys.exit(0)
 
-    ndims = 2
-    if sys.argv[1] == '-3':
+    if '-2' in sys.argv:
+        ndims = 2
+    if '-3' in sys.argv:
         ndims = 3
-    if sys.argv[1] == '-3' or sys.argv[1] == '-2':
-        del sys.argv[1]
+    if '-a' in sys.argv:
+        output_in_binary = False
+    if '-t' in sys.argv:
+        output_tensor_components = True
+
+    # delete options
+    for i in range(len(sys.argv)):
+        if sys.argv[1][0] == '-':
+            del sys.argv[1]
+        else:
+            # the rest of argv cannot be options
+            break
 
     modelname = sys.argv[1]
 
@@ -193,4 +283,4 @@ if __name__ == '__main__':
     else:
         end = int(sys.argv[3]) + 1
 
-    main(ndims, modelname, start, end)
+    main(modelname, start, end)
