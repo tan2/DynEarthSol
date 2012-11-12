f6e62e5501201cf9948027bbb635ec8f771e38bb
Eh Tan <tan2@mail.utexas.edu>
2012-11-12 11:57:34 +0800

    clarify parameters

--- input.cxx
+++ input.cxx
@@ -36,7 +36,7 @@ static void declare_parameters(po::options_description &cfg,
         ("mesh.xlength", po::value<double>(&p.mesh.xlength)->required(),
          "Length of x (in meters)")
         ("mesh.ylength", po::value<double>(&p.mesh.ylength)->required(),
-         "Length of y (in meters)")
+         "Length of y (in meters), for 3D only")
         ("mesh.zlength", po::value<double>(&p.mesh.zlength)->required(),
          "Length of z (in meters)")
 
@@ -44,21 +44,21 @@ static void declare_parameters(po::options_description &cfg,
          "Spatial resolution (in meters)")
         // for 2D only
         ("mesh.min_angle", po::value<double>(&p.mesh.min_angle)->default_value(32.),
-         "Min. angle of all triangles (in degrees)")
+         "Min. angle of all triangles (in degrees), for 2D only")
         // for 3D only
         ("mesh.min_tet_angle", po::value<double>(&p.mesh.min_tet_angle)->default_value(22.),
-         "Min. dihedral angle of all tetrahedra (in degrees)")
+         "Min. dihedral angle of all tetrahedra (in degrees), for 3D only")
         ("mesh.max_ratio", po::value<double>(&p.mesh.max_ratio)->default_value(2.),
-         "Max. radius / length ratio of all tetrahedra")
+         "Max. radius / length ratio of all tetrahedra, for 3D only")
 
         // for meshing_option = 2 only
         /* read the value as string, then parse as two numbers later */
         ("mesh.refined_zonex", po::value<std::string>(),
-         "Refining portion of xlength ([d0,d1]; 0<=d0<=d1<=1)")
+         "Refining portion of xlength ([d0,d1]; 0<=d0<=d1<=1), for meshing_option=2 only")
         ("mesh.refined_zoney", po::value<std::string>(),
-         "Refining portion of ylength ([d0,d1]; 0<=d0<=d1<=1)")
+         "Refining portion of ylength ([d0,d1]; 0<=d0<=d1<=1), for meshing_option=2 only, for 3D only")
         ("mesh.refined_zonez", po::value<std::string>(),
-         "Refining portion of zlength ([d0,d1]; 0<=d0<=d1<=1)")
+         "Refining portion of zlength ([d0,d1]; 0<=d0<=d1<=1), for meshing_option=2 only")
         ;
 
     cfg.add_options()
