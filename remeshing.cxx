601954594e9731782e5567351c5c0e04e66a673e
Eh Tan <tan2@mail.utexas.edu>
2013-01-16 17:23:23 -0600

    normalizing q so that its magnitude is about the same in 2D and 3D

--- remeshing.cxx
+++ remeshing.cxx
@@ -12,6 +12,10 @@ bool bad_mesh_quality(const Param &param, const Variables &var)
     int worst_elem;
     double q = worst_elem_quality(*var.coord, *var.connectivity,
                                   *var.volume, worst_elem);
+#ifdef THREED
+    // normalizing q so that its magnitude is about the same in 2D and 3D
+    q = std::pow(q, 1.0/3);
+#endif
     std::cout << "Worst mesh quality = " << q << " at element #" << worst_elem << ".\n";
     if (q < param.mesh.min_quality) {
         return 1;
