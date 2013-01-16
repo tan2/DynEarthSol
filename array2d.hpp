b58ba8f3329caa75bd778182b5a36f8a64dee2f6
Eh Tan <tan2@mail.utexas.edu>
2013-01-16 17:23:23 -0600

    prevent deleting pointer that was malloc'd

--- array2d.hpp
+++ array2d.hpp
@@ -9,27 +9,33 @@ class Array2D {
 
     T* a_;
     int n_;
+    // Can this object safely delete a_?
+    // When a_ is owned by another object, or when a_ is mallac'd (not
+    // returned by the new operator), we cannot delete a_ in this object.
+    bool can_delete;
 
 public:
     //
     // constructors & destructor
     //
-    Array2D() {a_ = NULL; n_ = 0;}
-    Array2D(T* a, int n) {a_ = a; n_ = n;}
+    Array2D() {a_ = NULL; n_ = 0; can_delete = 1;}
+    Array2D(T* a, int n) {a_ = a; n_ = n; can_delete = 0;}
 
     Array2D(int size, const T& val) {
         a_ = new T[N*size];
         n_ = size;
         std::fill_n(a_, N*n_, val);
+        can_delete = 1;
     }
 
     explicit
     Array2D(int size) {
         a_ = new T[N*size];
         n_ = size;
+        can_delete = 1;
     }
 
-    ~Array2D() {delete [] a_;}
+    ~Array2D() {if (can_delete) delete [] a_;}
 
     //
     // methods
@@ -40,13 +46,14 @@ public:
     int num_elements() const {return N*n_;}
 
     // NOTE: reset DOES NOT delete the array
-    void reset() {a_ = NULL; n_ = 0;}
+    void reset() {a_ = NULL; n_ = 0; can_delete = 0;}
 
     // steal the pointer from other
     void steal_ref(Array2D& other) {
-        delete [] a_;
+        if (can_delete) delete [] a_;
         a_ = other.data();
         n_ = other.size();
+        can_delete = other.can_delete;
         other.reset();
     }
 
