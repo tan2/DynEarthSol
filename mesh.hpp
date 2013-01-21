279fbb9f7e2cf48c7f8f6b608d2807727ad39370
Eh Tan <tan2@mail.utexas.edu>
2013-01-21 16:10:23 +0800

    added function prototypes that will be used in remeshing

--- mesh.hpp
+++ mesh.hpp
@@ -5,7 +5,10 @@ void points_to_mesh(const Param &param, Variables &var,
                     int npoints, double *points,
                     int n_init_segments, int *init_segments, int *init_segflags,
                     double max_elem_size, int vertex_per_polygon);
-
+void create_boundary_flags(Variables& var);
+void create_boundary_facets(Variables& var);
+void create_support(Variables& var);
+void create_elem_groups(Variables& var);
 void create_new_mesh(const Param&, Variables&);
 
 #endif
