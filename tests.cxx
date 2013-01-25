23070bd42c67a8502f4bcd0325d99f3b9c55e19b
Eh Tan <tan2@mail.utexas.edu>
2013-01-25 16:12:46 +0800

    added tests for array2d and sortindex

--- tests.cxx
+++ tests.cxx
@@ -1,8 +1,33 @@
+#include <algorithm>
+
 #include "constants.hpp"
 #include "parameters.hpp"
 
+#include "array2d.hpp"
 #include "barycentric-fn.hpp"
+#include "sortindex.hpp"
 #include "utils.hpp"
+
+
+void test_array2d()
+{
+
+    Array2D<int,3> a(10);
+    {
+        int *r = new int[10*3];
+        Array2D<int,3> a(r, 10);
+    }
+    for (int i=0; i<10; i++) {
+        int *b = a[i];
+        for (int j=0; j<3; j++)
+            b[j] = i+j;
+    }
+
+    int *c = a.data();
+    for (int i=0; i<10*3; i++) {
+        std::cout << *(c+i) << '\n';
+    }
+}
 
 
 void test_barycentric_transformation(Variables &var)
@@ -30,3 +55,21 @@ void test_barycentric_transformation(Variables &var)
 }
 
 
+void test_sortindex()
+{
+    std::vector<std::size_t> list(10), idx(10);
+    std::iota(list.begin(), list.begin()+5, 10);
+    std::iota(list.begin()+5, list.end(), 0);
+
+    sortindex(list, idx);
+
+    std::cout << "result ...\n";
+    for(int i=0; i<10; ++i) {
+        std::cout << list[i] << ", " << idx[i] << '\n';
+    }
+
+    std::cout << "sorted ...\n";
+    for(int i=0; i<10; ++i) {
+        std::cout << i << ": " << list[idx[i]] << '\n';
+    }
+}
