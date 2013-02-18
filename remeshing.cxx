730c851e6908cfc4557d0e28636439eee284bfd8
Eh Tan <tan2@mail.utexas.edu>
2013-02-18 14:58:20 +0800

    Array2D::can_delete is not necessary anymore

--- remeshing.cxx
+++ remeshing.cxx
@@ -222,11 +222,6 @@ void remesh(const Param &param, Variables &var)
     update_strain_rate(var, *var.strain_rate);
     update_force(param, var, *var.force);
 
-    delete [] old_coord.data();
-    delete [] old_connectivity.data();
-    delete [] old_segment.data();
-    delete [] old_segflag.data();
-
     std::cout << "  Remeshing finished.\n";
 }
 
