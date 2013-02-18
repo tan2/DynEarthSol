8a58c825cd58aad839badfd97c06fd3058e36fc1
Eh Tan <tan2@mail.utexas.edu>
2013-02-18 14:58:31 +0800

    refactored to a function

--- remeshing.cxx
+++ remeshing.cxx
@@ -112,47 +112,13 @@ void delete_points(const int_vec &to_delete, const array_t &old_coord,
     }
 }
 
-}
 
-
-bool bad_mesh_quality(const Param &param, const Variables &var)
+void new_mesh(const Param &param, Variables &var,
+              const array_t &old_coord, const conn_t &old_connectivity,
+              const segment_t &old_segment, const segflag_t &old_segflag)
 {
-    int worst_elem;
-    double q = worst_elem_quality(*var.coord, *var.connectivity,
-                                  *var.volume, worst_elem);
-#ifdef THREED
-    // normalizing q so that its magnitude is about the same in 2D and 3D
-    q = std::pow(q, 1.0/3);
-#endif
-    std::cout << "Worst mesh quality = " << q << " at element #" << worst_elem << ".\n";
-    if (q < param.mesh.min_quality) {
-        return 1;
-    }
-    return 0;
-}
-
-
-void remesh(const Param &param, Variables &var)
-{
-    std::cout << "  Remeshing starts...\n";
-
-    // setting up barycentric transformation
-
-    // saving the mesh to .poly file
-
-    // creating a "copy" of mesh pointer so that they are not deleted
-    int old_nnode = var.nnode;
-    int old_nseg = var.nseg;
-    array_t old_coord;
-    conn_t old_connectivity;
-    segment_t old_segment;
-    segflag_t old_segflag;
-    old_coord.steal_ref(*var.coord);
-    old_connectivity.steal_ref(*var.connectivity);
-    old_segment.steal_ref(*var.segment);
-    old_segflag.steal_ref(*var.segflag);
-
-    // XXX: modifying boundary if necessary
+    int old_nnode = old_coord.size();
+    int old_nseg = old_segment.size();
 
     // We don't want to refine large elements during remeshing,
     // so using the domain size as the max area
@@ -219,10 +185,49 @@ void remesh(const Param &param, Variables &var)
     var.connectivity->steal_ref(new_connectivity);
     var.segment->reset(psegment, var.nseg);
     var.segflag->reset(psegflag, var.nseg);
+}
 
-    // interpolating fields
-    nearest_neighbor_interpolation(var, old_coord, old_connectivity);
-    barycentric_node_interpolation(var, old_coord, old_connectivity);
+}
+
+
+bool bad_mesh_quality(const Param &param, const Variables &var)
+{
+    int worst_elem;
+    double q = worst_elem_quality(*var.coord, *var.connectivity,
+                                  *var.volume, worst_elem);
+#ifdef THREED
+    // normalizing q so that its magnitude is about the same in 2D and 3D
+    q = std::pow(q, 1.0/3);
+#endif
+    std::cout << "Worst mesh quality = " << q << " at element #" << worst_elem << ".\n";
+    if (q < param.mesh.min_quality) {
+        return 1;
+    }
+    return 0;
+}
+
+
+void remesh(const Param &param, Variables &var)
+{
+    std::cout << "  Remeshing starts...\n";
+
+    {
+        // creating a "copy" of mesh pointer so that they are not deleted
+        array_t old_coord;
+        conn_t old_connectivity;
+        segment_t old_segment;
+        segflag_t old_segflag;
+        old_coord.steal_ref(*var.coord);
+        old_connectivity.steal_ref(*var.connectivity);
+        old_segment.steal_ref(*var.segment);
+        old_segflag.steal_ref(*var.segflag);
+
+        new_mesh(param, var, old_coord, old_connectivity, old_segment, old_segflag);
+
+        // interpolating fields
+        nearest_neighbor_interpolation(var, old_coord, old_connectivity);
+        barycentric_node_interpolation(var, old_coord, old_connectivity);
+    }
 
     // memory for new fields
     reallocate_variables(param, var);
