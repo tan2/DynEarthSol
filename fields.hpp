d3bd7123007c17e53f9ac62de00af9f4bef649a0
Eh Tan <tan2@mail.utexas.edu>
2013-02-18 14:58:13 +0800

    forgot to update the header

--- fields.hpp
+++ fields.hpp
@@ -6,7 +6,6 @@ void reallocate_variables(const Param &param, Variables& var);
 void update_temperature(const Param &param, const Variables &var,
                         double_vec &temperature, double_vec &tdot);
 void update_strain_rate(const Variables& var, tensor_t& strain_rate);
-void update_spin_rate(const Variables& var, tensor_t& spin_rate);
 void update_force(const Param& param, const Variables& var, array_t& force);
 void update_velocity(const Variables& var, array_t& vel);
 void update_coordinate(const Variables& var, array_t& coord);
