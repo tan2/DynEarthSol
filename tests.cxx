2033990b23eb2daf7bc76f02f3446fc4e126094b
Eh Tan <tan2@mail.utexas.edu>
2014-05-16 14:48:03 +0800

    added tests for principal stresses

--- tests.cxx
+++ tests.cxx
@@ -9,27 +9,6 @@
 #include "utils.hpp"
 
 
-void test_array2d()
-{
-
-    Array2D<int,3> a(10);
-    {
-        int *r = new int[10*3];
-        Array2D<int,3> a(r, 10);
-    }
-    for (int i=0; i<10; i++) {
-        int *b = a[i];
-        for (int j=0; j<3; j++)
-            b[j] = i+j;
-    }
-
-    int *c = a.data();
-    for (int i=0; i<10*3; i++) {
-        std::cout << *(c+i) << '\n';
-    }
-}
-
-
 void test_barycentric_transformation(Variables &var)
 {
     Barycentric_transformation bary(*var.coord, *var.connectivity, *var.volume);
@@ -52,6 +31,27 @@ void test_barycentric_transformation(Variables &var)
     // or "[0.25, 0.25, 0.25]" in 3D
     print(std::cout, q, NDIMS);
     std::cout << '\n';
+}
+
+
+void test_array2d()
+{
+
+    Array2D<int,3> a(10);
+    {
+        int *r = new int[10*3];
+        Array2D<int,3> a(r, 10);
+    }
+    for (int i=0; i<10; i++) {
+        int *b = a[i];
+        for (int j=0; j<3; j++)
+            b[j] = i+j;
+    }
+
+    int *c = a.data();
+    for (int i=0; i<10*3; i++) {
+        std::cout << *(c+i) << '\n';
+    }
 }
 
 
@@ -127,3 +127,106 @@ void test_eigen_decomposition()
 }
 
 
+void test_principal_stresses3()
+{
+    /* To run this test, you will have to copy rheology.cxx::principal_stresses3()
+     * into this file.
+     */
+
+    double a[6] = {0};
+    a[0] = 3e4;
+    a[1] = -1e-5;
+    a[2] = 3;
+    a[3] = 2;
+    a[4] = 4;
+    a[5] = 8;
+
+    std::cout << "\noriginal matrix:";
+    print(std::cout, a, 6);
+    std::cout << "\n";
+
+    // p is eigenvalue, v is eigenvector
+    double p[3], v[3][3];
+
+    principal_stresses3(a, p, v);
+
+    std::cout << "eigenvalue:";
+    print(std::cout, p, 3);
+    std::cout << "\neigenvector:";
+    print(std::cout, v[0], 3);
+    print(std::cout, v[1], 3);
+    print(std::cout, v[2], 3);
+    std::cout << "\n";
+
+    double ss[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+    for(int m=0; m<3; m++) {
+        //for(int n=0; n<3; n++) { // using this loop to recover whole matrix
+        for(int n=m; n<3; n++) { // using this loop to recover upper half matrix
+            for(int k=0; k<3; k++) {
+                ss[m][n] += v[m][k] * v[n][k] * p[k];
+            }
+        }
+    }
+
+    double b[6];
+    b[0] = ss[0][0];
+    b[1] = ss[1][1];
+    b[2] = ss[2][2];
+    b[3] = ss[0][1];
+    b[4] = ss[0][2];
+    b[5] = ss[1][2];
+
+    std::cout << "\nmatrix:";
+    print(std::cout, b, 6);
+    std::cout << "\n";
+}
+
+
+void test_principal_stresses2()
+{
+    /* To run this test, you will have to copy rheology.cxx::principal_stresses2()
+     * into this file.
+     */
+
+    double a[3] = {0};
+    a[0] = -3;
+    a[1] = 1;
+    a[2] = 4;
+
+    std::cout << "\noriginal matrix:";
+    print(std::cout, a, 3);
+    std::cout << "\n";
+
+    // p is eigenvalue, v is eigenvector
+    double p[2], cos2t, sin2t;
+
+    principal_stresses2(a, p, cos2t, sin2t);
+
+    std::cout << "eigenvalue:";
+    print(std::cout, p, 2);
+    std::cout << "\n"
+              << cos2t << ' ' << sin2t << '\n';
+
+    double s[3];
+
+    double dc2 = (p[0] - p[1]) * cos2t;
+    double dss = p[0] + p[1];
+    s[0] = 0.5 * (dss + dc2);
+    s[1] = 0.5 * (dss - dc2);
+    s[2] = 0.5 * (p[0] - p[1]) * sin2t;
+
+    std::cout << "\nmatrix:";
+    print(std::cout, s, 3);
+    std::cout << "\n";
+}
+
+// Compile with:
+//    g++ --std=c++11 tests.cxx 3x3-C/lib3x3.a
+//
+// int main(int, char**)
+// {
+//     // test_array2d();
+//     // test_sortindex();
+//     // test_principal_stresses3();
+//     // test_principal_stresses2();
+// }
