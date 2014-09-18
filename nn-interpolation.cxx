ba477d1b91f2a6f3227f6d3a704af96a5bbde6af
Eh Tan <tan2@mail.utexas.edu>
2014-09-18 12:54:43 +0800

    put static function inside anonymous namespace

--- nn-interpolation.cxx
+++ nn-interpolation.cxx
@@ -8,88 +8,92 @@
 #include "nn-interpolation.hpp"
 
 
-void find_nearest_neighbor(Variables &var, const array_t &old_coord,
-                           const conn_t &old_connectivity, int_vec &idx)
-{
-    std::cout << "Constructing a kd-tree.\n";
-    // kdtree requires the coordinate as double**
-    double **old_center = elem_center(old_coord, old_connectivity);
-    ANNkd_tree kdtree(old_center, old_connectivity.size(), NDIMS);
+namespace {
 
-    std::cout << "Searching nearest neighbor in the kd-tree.\n";
-    double **new_center = elem_center(*var.coord, *var.connectivity);
+    void find_nearest_neighbor(Variables &var, const array_t &old_coord,
+                               const conn_t &old_connectivity, int_vec &idx)
+    {
+        std::cout << "Constructing a kd-tree.\n";
+        // kdtree requires the coordinate as double**
+        double **old_center = elem_center(old_coord, old_connectivity);
+        ANNkd_tree kdtree(old_center, old_connectivity.size(), NDIMS);
 
-    const int k = 1;
-    const double eps = 0;
-    int *nn_idx = new int[k];
-    double *dd = new double[k];
-    // Note: kdtree.annkSearch() is not thread-safe, cannot use openmp in this loop
-    for(int e=0; e<var.nelem; e++) {
-        double *q = new_center[e];
-        kdtree.annkSearch(q, k, nn_idx, dd, eps);
-        idx[e] = nn_idx[0];
+        std::cout << "Searching nearest neighbor in the kd-tree.\n";
+        double **new_center = elem_center(*var.coord, *var.connectivity);
+
+        const int k = 1;
+        const double eps = 0;
+        int *nn_idx = new int[k];
+        double *dd = new double[k];
+        // Note: kdtree.annkSearch() is not thread-safe, cannot use openmp in this loop
+        for(int e=0; e<var.nelem; e++) {
+            double *q = new_center[e];
+            kdtree.annkSearch(q, k, nn_idx, dd, eps);
+            idx[e] = nn_idx[0];
+        }
+
+        delete [] nn_idx;
+        delete [] dd;
+        delete [] new_center[0];
+        delete [] new_center;
+        delete [] old_center[0];
+        delete [] old_center;
     }
 
-    delete [] nn_idx;
-    delete [] dd;
-    delete [] new_center[0];
-    delete [] new_center;
-    delete [] old_center[0];
-    delete [] old_center;
-}
 
-
-static void inject_field(const int_vec &idx, const double_vec &source, double_vec &target)
-{
-    #pragma omp parallel for default(none)          \
-        shared(idx, source, target)
-    for (std::size_t i=0; i<target.size(); i++) {
-        int n = idx[i];
-        target[i] = source[n];
-    }
-}
-
-
-static void inject_field(const int_vec &idx, const tensor_t &source, tensor_t &target)
-{
-    #pragma omp parallel for default(none)          \
-        shared(idx, source, target)
-    for (std::size_t i=0; i<target.size(); i++) {
-        int n = idx[i];
-        for (int d=0; d<NSTR; d++) {
-            target[i][d] = source[n][d];
+    void inject_field(const int_vec &idx, const double_vec &source, double_vec &target)
+    {
+        #pragma omp parallel for default(none)          \
+            shared(idx, source, target)
+        for (std::size_t i=0; i<target.size(); i++) {
+            int n = idx[i];
+            target[i] = source[n];
         }
     }
-}
 
 
-static void nn_interpolate_elem_fields(Variables &var, const int_vec &idx)
-{
-    const int n = var.nnode;
-    const int e = var.nelem;
+    void inject_field(const int_vec &idx, const tensor_t &source, tensor_t &target)
+    {
+        #pragma omp parallel for default(none)          \
+            shared(idx, source, target)
+        for (std::size_t i=0; i<target.size(); i++) {
+            int n = idx[i];
+            for (int d=0; d<NSTR; d++) {
+                target[i][d] = source[n][d];
+            }
+        }
+    }
 
-    double_vec *a;
 
-    a = new double_vec(e);
-    inject_field(idx, *var.plstrain, *a);
-    delete var.plstrain;
-    var.plstrain = a;
+    void nn_interpolate_elem_fields(Variables &var, const int_vec &idx)
+    {
+        const int n = var.nnode;
+        const int e = var.nelem;
 
-    a = new double_vec(e);
-    inject_field(idx, *var.delta_plstrain, *a);
-    delete var.delta_plstrain;
-    var.delta_plstrain = a;
+        double_vec *a;
 
-    tensor_t *b;
-    b = new tensor_t(e);
-    inject_field(idx, *var.strain, *b);
-    delete var.strain;
-    var.strain = b;
+        a = new double_vec(e);
+        inject_field(idx, *var.plstrain, *a);
+        delete var.plstrain;
+        var.plstrain = a;
 
-    b = new tensor_t(e);
-    inject_field(idx, *var.stress, *b);
-    delete var.stress;
-    var.stress = b;
+        a = new double_vec(e);
+        inject_field(idx, *var.delta_plstrain, *a);
+        delete var.delta_plstrain;
+        var.delta_plstrain = a;
+
+        tensor_t *b;
+        b = new tensor_t(e);
+        inject_field(idx, *var.strain, *b);
+        delete var.strain;
+        var.strain = b;
+
+        b = new tensor_t(e);
+        inject_field(idx, *var.stress, *b);
+        delete var.stress;
+        var.stress = b;
+
+    }
 
 }
 
