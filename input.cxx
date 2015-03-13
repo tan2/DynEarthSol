3cd8e4028a7bcb2dc94b9f275caefa68e652771b
Eh Tan <tan2@mail.utexas.edu>
2015-03-13 12:56:45 +0800

    Setting mesh.is_discarding_internal_segments default to true.

--- input.cxx
+++ input.cxx
@@ -133,7 +133,7 @@ static void declare_parameters(po::options_description &cfg,
          "10: no modification on any boundary, except small boundary segments might get merged.\n"
          "11: move all bottom nodes to initial depth, other boundaries are intact, small boundary segments might get merged.\n")
 
-        ("mesh.is_discarding_internal_segments", po::value<bool>(&p.mesh.is_discarding_internal_segments)->default_value(false),
+        ("mesh.is_discarding_internal_segments", po::value<bool>(&p.mesh.is_discarding_internal_segments)->default_value(true),
          "Discarding internal segments after initial mesh is created? "
          "Using it when remeshing process can modify segments (e.g. remeshing_option=11).")
 
