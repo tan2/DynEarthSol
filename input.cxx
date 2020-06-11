679bddf04d74bbcaaa61422591200618ee314637
Eh Tan <tan2@earth.sinica.edu.tw>
2020-06-11 16:38:57 +0800

    Fix a typo

--- input.cxx
+++ input.cxx
@@ -529,7 +529,7 @@ static void get_numbers(const po::variables_map &vm, const char *name,
 
 static void validate_parameters(const po::variables_map &vm, Param &p)
 {
-    std::cout << "Checking consisitency of input parameters...\n";
+    std::cout << "Checking consistency of input parameters...\n";
 
     //
     // stopping condition and output interval are based on either model time or step
