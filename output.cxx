ef1b8f8e9dfe5138ea000340b0929f0d9ea88e42
Chase J. Shyu <iamhemry@gmail.com>
2021-08-25 09:55:39 +0800

    Disable omp nowait for output.

--- output.cxx
+++ output.cxx
@@ -89,154 +89,133 @@ void Output::write(const Variables& var, bool is_averaged)
         dt = (var.time - time0) / average_interval;
         inv_dt = 1.0 / (var.time - time0);
     }
-    #pragma omp parallel
-    {
-        #pragma omp single nowait
-        {
-            write_info(var, dt);
+
+    write_info(var, dt);
+
+    char filename[256];
+    std::snprintf(filename, 255, "%s.save.%06d", modelname.c_str(), frame);
+    BinaryOutput bin(filename);
+
+    bin.write_array(*var.coord, "coordinate", var.coord->size());
+    bin.write_array(*var.connectivity, "connectivity", var.connectivity->size());
+
+    bin.write_array(*var.vel, "velocity", var.vel->size());
+    if (average_interval && is_averaged) {
+        // average_velocity = displacement / delta_t
+        double *c0 = coord0.data();
+        const double *c = var.coord->data();
+        for (int i=0; i<coord0.num_elements(); ++i) {
+            c0[i] = (c[i] - c0[i]) * inv_dt;
         }
+        bin.write_array(coord0, "velocity averaged", coord0.size());
+    }
 
-        #pragma omp single nowait
-        {
-            char filename[256];
-            std::snprintf(filename, 255, "%s.save.%06d", modelname.c_str(), frame);
-            BinaryOutput bin(filename);
+    bin.write_array(*var.temperature, "temperature", var.temperature->size());
 
-            bin.write_array(*var.coord, "coordinate", var.coord->size());
-            bin.write_array(*var.connectivity, "connectivity", var.connectivity->size());
+    bin.write_array(*var.plstrain, "plastic strain", var.plstrain->size());
 
-            bin.write_array(*var.vel, "velocity", var.vel->size());
-            if (average_interval && is_averaged) {
-                // average_velocity = displacement / delta_t
-                double *c0 = coord0.data();
-                const double *c = var.coord->data();
-                for (int i=0; i<coord0.num_elements(); ++i) {
-                    c0[i] = (c[i] - c0[i]) * inv_dt;
-                }
-                bin.write_array(coord0, "velocity averaged", coord0.size());
-            }
+    // Strain rate and plastic strain rate do not need to be checkpointed,
+    // so we don't have to distinguish averged/non-averaged variants.
+    double_vec *delta_plstrain = var.delta_plstrain;
+    if (average_interval && is_averaged) {
+        // average_strain_rate = delta_strain / delta_t
+        delta_plstrain = &delta_plstrain_avg;
+    }
+    for (std::size_t i=0; i<delta_plstrain_avg.size(); ++i) {
+        delta_plstrain_avg[i] *= inv_dt;
+    }
+    bin.write_array(*delta_plstrain, "plastic strain-rate", delta_plstrain->size());
 
-            bin.write_array(*var.temperature, "temperature", var.temperature->size());
-
-            bin.write_array(*var.plstrain, "plastic strain", var.plstrain->size());
-
-            // Strain rate and plastic strain rate do not need to be checkpointed,
-            // so we don't have to distinguish averged/non-averaged variants.
-            double_vec *delta_plstrain = var.delta_plstrain;
-            if (average_interval && is_averaged) {
-                // average_strain_rate = delta_strain / delta_t
-                delta_plstrain = &delta_plstrain_avg;
-            }
-            for (std::size_t i=0; i<delta_plstrain_avg.size(); ++i) {
-                delta_plstrain_avg[i] *= inv_dt;
-            }
-            bin.write_array(*delta_plstrain, "plastic strain-rate", delta_plstrain->size());
-
-            tensor_t *strain_rate = var.strain_rate;
-            if (average_interval && is_averaged) {
-                // average_strain_rate = delta_strain / delta_t
-                strain_rate = &strain0;
-                double *s0 = strain0.data();
-                const double *s = var.strain->data();
-                for (int i=0; i<strain0.num_elements(); ++i) {
-                    s0[i] = (s[i] - s0[i]) * inv_dt;
-                }
-            }
-            bin.write_array(*strain_rate, "strain-rate", strain_rate->size());
-
-            bin.write_array(*var.strain, "strain", var.strain->size());
-            bin.write_array(*var.stress, "stress", var.stress->size());
-
-            if (average_interval && is_averaged) {
-                double *s = stress_avg.data();
-                double tmp = 1.0 / (average_interval + 1);
-                for (int i=0; i<stress_avg.num_elements(); ++i) {
-                    s[i] *= tmp;
-                }
-                bin.write_array(stress_avg, "stress averaged", stress_avg.size());
-            }
-
-            double_vec tmp(var.nelem);
-            #pragma omp parallel for default(none) shared(var, tmp)
-            for (int e=0; e<var.nelem; ++e) {
-                tmp[e] = var.mat->rho(e);
-            }
-            bin.write_array(tmp, "density", tmp.size());
-
-            #pragma omp parallel for default(none) shared(var, tmp)
-            for (int e=0; e<var.nelem; ++e) {
-                tmp[e] = elem_quality(*var.coord, *var.connectivity, *var.volume, e);
-            }
-            bin.write_array(tmp, "mesh quality", tmp.size());
-
-            #pragma omp parallel for default(none) shared(var, tmp)
-            for (int e=0; e<var.nelem; ++e) {
-                tmp[e] = var.mat->visc(e);
-            }
-            bin.write_array(tmp, "viscosity", tmp.size());
-            // bin.write_array(*var.mass, "mass", var.mass->size());
-            // bin.write_array(*var.tmass, "tmass", var.tmass->size());
-            // bin.write_array(*var.volume_n, "volume_n", var.volume_n->size());
-            // bin.write_array(*var.volume, "volume", var.volume->size());
-            // bin.write_array(*var.edvoldt, "edvoldt", var.edvoldt->size());
-
-            #pragma omp parallel for default(none) shared(var, tmp)
-            for (int e=0; e<var.nelem; ++e) {
-                // Find the most abundant marker mattype in this element
-                int_vec &a = (*var.elemmarkers)[e];
-                tmp[e] = std::distance(a.begin(), std::max_element(a.begin(), a.end()));
-            }
-            bin.write_array(tmp, "material", tmp.size());
-
-            bin.write_array(*var.force, "force", var.force->size());
-
-            bin.write_array(*var.coord0, "coord0", var.coord0->size());
-
-            bin.write_array(*var.bcflag, "bcflag", var.bcflag->size());
-
-            if (has_marker_output) {
-                for (auto ms=var.markersets.begin(); ms!=var.markersets.end(); ++ms)
-                    (*ms)->write_save_file(var, bin);
-            }
-
-            bin.close();
-        }
-
-        #pragma omp single nowait
-        {
-//#ifdef USE_NPROF
-//            nvtxRangePushA("print_close");
-//#endif
-            std::cout << "  Output # " << frame
-                        << ", step = " << var.steps
-                        << ", time = " << var.time / YEAR2SEC << " yr"
-                        << ", dt = " << dt / YEAR2SEC << " yr.\n";
-
-            frame ++;
-
-            {
-                // check for NaN in coordinate
-                for (int i=0; i<var.nnode; i++)
-                    for (int j=0; j<NDIMS; j++) {
-                        if (std::isnan((*var.coord)[i][j])) {
-                            std::cerr << "Error: coordinate becomes NaN\n";
-                            std::exit(11);
-                        }
-                        if (std::isinf((*var.coord)[i][j])) {
-                            std::cerr << "Error: coordinate becomes Infinity\n";
-                            std::exit(11);
-                        }
-                    }
-            }
-//#ifdef USE_NPROF
-//            nvtxRangePop();
-//#endif
+    tensor_t *strain_rate = var.strain_rate;
+    if (average_interval && is_averaged) {
+        // average_strain_rate = delta_strain / delta_t
+        strain_rate = &strain0;
+        double *s0 = strain0.data();
+        const double *s = var.strain->data();
+        for (int i=0; i<strain0.num_elements(); ++i) {
+            s0[i] = (s[i] - s0[i]) * inv_dt;
         }
     }
-//#ifdef USE_NPROF
-//    nvtxRangePop();
-//#endif
+    bin.write_array(*strain_rate, "strain-rate", strain_rate->size());
 
+    bin.write_array(*var.strain, "strain", var.strain->size());
+    bin.write_array(*var.stress, "stress", var.stress->size());
+
+    if (average_interval && is_averaged) {
+        double *s = stress_avg.data();
+        double tmp = 1.0 / (average_interval + 1);
+        for (int i=0; i<stress_avg.num_elements(); ++i) {
+            s[i] *= tmp;
+        }
+        bin.write_array(stress_avg, "stress averaged", stress_avg.size());
+    }
+
+    double_vec tmp(var.nelem);
+    #pragma omp parallel for default(none) shared(var, tmp)
+    for (int e=0; e<var.nelem; ++e) {
+        tmp[e] = var.mat->rho(e);
+    }
+    bin.write_array(tmp, "density", tmp.size());
+
+    #pragma omp parallel for default(none) shared(var, tmp)
+    for (int e=0; e<var.nelem; ++e) {
+        tmp[e] = elem_quality(*var.coord, *var.connectivity, *var.volume, e);
+    }
+    bin.write_array(tmp, "mesh quality", tmp.size());
+
+    #pragma omp parallel for default(none) shared(var, tmp)
+    for (int e=0; e<var.nelem; ++e) {
+        tmp[e] = var.mat->visc(e);
+    }
+    bin.write_array(tmp, "viscosity", tmp.size());
+    // bin.write_array(*var.mass, "mass", var.mass->size());
+    // bin.write_array(*var.tmass, "tmass", var.tmass->size());
+    // bin.write_array(*var.volume_n, "volume_n", var.volume_n->size());
+    // bin.write_array(*var.volume, "volume", var.volume->size());
+    // bin.write_array(*var.edvoldt, "edvoldt", var.edvoldt->size());
+
+    #pragma omp parallel for default(none) shared(var, tmp)
+    for (int e=0; e<var.nelem; ++e) {
+        // Find the most abundant marker mattype in this element
+        int_vec &a = (*var.elemmarkers)[e];
+        tmp[e] = std::distance(a.begin(), std::max_element(a.begin(), a.end()));
+    }
+    bin.write_array(tmp, "material", tmp.size());
+
+    bin.write_array(*var.force, "force", var.force->size());
+
+    bin.write_array(*var.coord0, "coord0", var.coord0->size());
+
+    bin.write_array(*var.bcflag, "bcflag", var.bcflag->size());
+
+    if (has_marker_output) {
+        for (auto ms=var.markersets.begin(); ms!=var.markersets.end(); ++ms)
+            (*ms)->write_save_file(var, bin);
+    }
+
+    bin.close();
+
+    std::cout << "  Output # " << frame
+                << ", step = " << var.steps
+                << ", time = " << var.time / YEAR2SEC << " yr"
+                << ", dt = " << dt / YEAR2SEC << " yr.\n";
+
+    frame ++;
+
+    {
+        // check for NaN in coordinate
+        for (int i=0; i<var.nnode; i++)
+            for (int j=0; j<NDIMS; j++) {
+                if (std::isnan((*var.coord)[i][j])) {
+                    std::cerr << "Error: coordinate becomes NaN\n";
+                    std::exit(11);
+                }
+                if (std::isinf((*var.coord)[i][j])) {
+                    std::cerr << "Error: coordinate becomes Infinity\n";
+                    std::exit(11);
+                }
+            }
+    }
 }
 
 
