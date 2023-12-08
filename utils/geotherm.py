dcd349cbb596053f6a72670908c3c90bb41abf58
Chase J. Shyu <iamhemry@gmail.com>
2023-12-08 15:35:16 +0800

    fix bug of avg k

--- utils/geotherm.py
+++ utils/geotherm.py
@@ -17,151 +17,127 @@ def continental_radiogenic_T(z,T0,hr,k,qm,rhoH0):
     T = T0 + qm/k*z + rhoH0*hr**2/k*(1-np.exp(-z/hr))
     return T
 
-def continental_radiogenic_T2(z,T0,thick_arr,hr,k_arr,qm,rhoH0_arr):
+def continental_radiogenic_T2(z,T0,bdy_arr,hr,k_arr,qm,rhoH0_arr):
     '''
     No heat production below the moho
     '''
-    dtlayer = np.array([dt_of_heat_production(thick_arr[i],k_arr[i],rhoH0_arr[i],hr) for i in range(len(thick_arr))])
-
-    dtlayer = dtlayer + qm*thick_arr/k_arr
+    dT_h_layer = dT_h(bdy_arr[:-1],bdy_arr[1:],k_arr,rhoH0_arr,hr)
+    dtlayer = dT_h_layer + (bdy_arr[1:]-bdy_arr[:-1])*qm/k_arr
     
     T = np.zeros_like(z)
-    t_arr_inv = thick_arr.copy()[::-1]
-    k_arr_inv = k_arr.copy()[::-1]
-    rhoH0_arr_inv = rhoH0_arr.copy()[::-1]
-    dtlayer_inv = dtlayer.copy()[::-1]
-
     for i, zt in enumerate(z):
-        for j in range(len(t_arr_inv)):
-            if zt <= np.sum(t_arr_inv[:j+1]):
-                dz = zt - np.sum(t_arr_inv[:j])
-                T[i] = T0 + np.sum(dtlayer_inv[:j]) + qm/k_arr_inv[j]*dz + rhoH0_arr_inv[j]*hr**2/k_arr_inv[j]*(1-np.exp(-dz/hr))
+        is_value = False
+        for j in range(len(k_arr)):
+            if zt <= bdy_arr[j+1]:
+                dTh = dT_h(bdy_arr[j],zt,k_arr[j],rhoH0_arr[j],hr)
+                T[i] = T0 + np.sum(dtlayer[:j]) + qm/k_arr[j]*(zt - bdy_arr[j]) + dTh
+                is_value = True
                 break
+        if not is_value:
+            raise ValueError(f'z={zt} is out of range')
     return T
 
-def dt_of_heat_production(thickness,k,rhoH0,hr):
-    return rhoH0*hr**2*(1-np.exp(-thickness/hr))/k
+def dT_h(zstart,zend,k,rhoH0,hr):
+    diff = np.exp(-zstart/hr) - np.exp(-zend/hr)
+    return diff*rhoH0*hr**2/k
 
-def sum_dt_of_layers(thick_arr,k_arr,rhoH0_arr,hr):
-    dt = 0
-    for i in range(len(thick_arr)):
-        dt += dt_of_heat_production(thick_arr[i],k_arr[i],rhoH0_arr[i],hr)
-    return dt
+def sum_dt_of_layers(bdy_arr,k_arr,rhoH0_arr,hr):
+    dt = dT_h(bdy_arr[:-1],bdy_arr[1:],k_arr,rhoH0_arr,hr)
+    return np.sum(dt)
 
 def avg_k(k_arr,thick_arr):
-    k = 0
-    for i in range(len(thick_arr)):
-        k += k_arr[i]*thick_arr[i]
-    return k/sum(thick_arr)
+    return np.sum(thick_arr)/np.sum(thick_arr/k_arr)
+
+def get_hz_profile(z,hr,rhoH0_arr,bdy_arr):
+    hz = np.zeros_like(z)
+    for i, zt in enumerate(z):
+        is_value = False
+        for j in range(len(rhoH0_arr)):
+            if zt <= bdy_arr[j+1]:
+                hz[i] = rhoH0_arr[j]*np.exp(-zt/hr)
+                is_value = True
+                break
+        if not is_value:
+            raise ValueError(f'z={zt} is out of range')
+    return hz
 
 def main():
-    k_arr = [3.3, 2.5] # W/mK thermal conductivity
-    rho_arr = [3300, 2800] # kg/m^3 density
-    cp_arr = [1000, 1000] # J/kgK specific heat capacity
-    H0_arr = [0, 6.0e-10] # W/kg heat production
-    H0 = 6.0e-10 # W/kg heat production
+    # material properties
+    mat_rho = [3300, 2800, 2800] # kg/m^3 density
+    mat_k = [3.3, 2.5, 2.5] # W/mK thermal conductivity
+    mat_cp = [1000, 1000, 1000] # J/kgK specific heat capacity
+    mat_H0 = [0, 1e-10,6.0e-10] # W/kg heat production
     # 9.6e-10 W/kg for granite
-    k_arr = [3.3, 2.5, 2.5] # W/mK thermal conductivity
-    rho_arr = [3300, 2800, 2800] # kg/m^3 density
-    cp_arr = [1000, 1000, 1000] # J/kgK specific heat capacity
-    H0_arr = [0, 1e-10,6.0e-10] # W/kg heat production
-    nlayer = len(k_arr)
-
-    k_arr = np.array(k_arr)
-    rho_arr = np.array(rho_arr)
-    cp_arr = np.array(cp_arr)
-    H0_arr = np.array(H0_arr)
-
-    alpha_arr = k_arr/rho_arr/cp_arr
-    rhoH0_arr = rho_arr * H0_arr
-    
-    Zbot = 120e3 # km
-    Zulb = 22e3 # km
-    Zmoho = 33e3 # km
-    thick_arr = [Zbot-Zmoho, Zmoho]
-    thick_arr = [Zbot-Zmoho, Zmoho-Zulb, Zulb]
-
-    thick_arr = np.array(thick_arr)
     hr = 33e3 # km length_scale_for_the_decrease_of_heat_production
 
-    # Brune 2014
-    rhoH0_arr[-2] = 0.2e-6 # W/m^3
-    rhoH0_arr[-1] = 1.5e-6 # W/m^3
+    mat_k = np.array(mat_k)
+    mat_rho = np.array(mat_rho)
+    mat_cp = np.array(mat_cp)
+    mat_H0 = np.array(mat_H0)
 
     T0 = 273
     Tm = 1300 + 273
-    
-    age = 26 # Myrs
 
+    Zulb = 22e3 # km
+    Zmoho = 33e3 # km
+    Zbot = 120e3 # km
+    
+    phase_arr = [2, 1, 0]
+    bdy_arr = [0, Zulb, Zmoho, Zbot]
+    bdy_arr = np.array(bdy_arr)
+    thick_arr = [bdy_arr[i+1]-bdy_arr[i] for i in range(len(bdy_arr)-1)]
+    thick_arr = np.array(thick_arr)
+    
+    k_arr = mat_k[phase_arr]
+    rho_arr = mat_rho[phase_arr]
+    H0_arr = mat_H0[phase_arr]
+    nlayer = len(phase_arr)
+
+    rhoH0_arr = rho_arr * H0_arr
+    # Brune 2014
+    rhoH0_arr[0] = 1.5e-6 # W/m^3
+    rhoH0_arr[1] = 0.2e-6 # W/m^3
+    
     z = np.linspace(0, Zbot, int(Zbot*0.1)+1)
 
-    th = half_space_cooling_T(z, T0, Tm, age,alpha_arr[0])
-
-    hz = rhoH0_arr[-1]*np.exp(-z/hr)
-
-    thick_arr_inv = thick_arr.copy()[::-1]
-    rhoH0_arr_inv = rhoH0_arr.copy()[::-1]
-    hz3 = np.zeros_like(z)
-    for i, zt in enumerate(z):
-        for j in range(len(thick_arr_inv)):
-            if zt < np.sum(thick_arr_inv[:j+1]):
-                dz = zt - np.sum(thick_arr_inv[:j])
-                hz3[i] = rhoH0_arr_inv[j]*np.exp(-dz/hr)
-                break
-    
-    
-    # heat flux generated by radiogenic heat production
-    qh = rhoH0_arr[-1]*hr**2*(1-np.exp(-Zbot/hr))/Zbot # W/m^3
-    q = (Tm-T0)/Zbot*k_arr[0]
-    
-    qm = 30e-3 # W/m^2 heat flux from mantle
-    qm = q - qh
-
-    tr = continental_radiogenic_T(z,T0,hr,k_arr[0],qm,rhoH0_arr[-1])
+    hz3 = get_hz_profile(z,hr,rhoH0_arr,bdy_arr)
     
     # calculate temperature increase by radiogenic heat production in the crust    
-    dT = sum_dt_of_layers(thick_arr,k_arr,rhoH0_arr,hr)
-    k = avg_k(k_arr,thick_arr)
-    qm3 = (Tm-T0-dT)/sum(thick_arr)*k
+    dT = sum_dt_of_layers(bdy_arr,k_arr,rhoH0_arr,hr)
+    qm = (Tm-T0-dT)/sum(thick_arr) * avg_k(k_arr,thick_arr)
     
-    tr3 = continental_radiogenic_T2(z,T0,thick_arr,hr,k_arr,qm3,rhoH0_arr)
+    temp = continental_radiogenic_T2(z,T0,bdy_arr,hr,k_arr,qm,rhoH0_arr)
 
-    print(f'temperature at surface: {tr[0]-273:.0f} degC')
-    print(f'temperature at moho: {np.interp(Zmoho,z,tr)-273:.0f} degC')
-    print(f'temperature at bottom: {tr[-1]-273:.0f} degC')
-    
+    print(f'temperature at surface: {temp[0]-273:.0f} degC')
+    print(f'temperature at moho: {np.interp(Zmoho,z,temp)-273:.0f} degC')
+    print(f'temperature at bottom: {temp[-1]-273:.0f} degC')
+
+    # plot
     fig, ax = plt.subplots(figsize=(4,8))
 
-    ax.hlines(hr*1e-3,0,1500,ls='--',color='grey')
-    ax.text(0.1,hr*1e-3+0.1,"hr",fontsize=12,color='grey')
-    ax.vlines(Tm-273,0,Zbot/1e3,ls='--',color='grey')
+    ax.vlines(Tm-273,0,Zbot/1e3,ls='--',color='grey',lw=1)
     ax.text(Tm-273+0.1,0.1,"Tm",fontsize=12,color='grey')
-    ax.hlines(Zmoho*1e-3,0,1500,ls='--',color='grey')
-    ax.text(0.1,Zmoho*1e-3+0.1,"Zmoho",fontsize=12,color='grey')
+    
+    for bdy in bdy_arr[1:-1]:
+        ax.hlines(bdy*1e-3,0,1500,ls='--',color='grey',lw=1)
 
-    ax.plot(th-273, z/1e3,'r',label="Half-space cooling")
+    color = 'k'
+    ax.plot(temp-273, z/1e3,'-',color=color,label="Radiogenic")
+    for bdy in bdy_arr[1:-1]:
+        tmp = np.interp(bdy,z,temp)
+        ax.plot([tmp-273],[bdy*1e-3],'o',color=color,ms=4,mfc='w')
+        ax.text(tmp-273+10,bdy*1e-3-1,f"{tmp-273:.0f}"+'$^\circ{C}$',fontsize=12,ha='left',va='bottom',color=color)
 
+    ax.plot([temp[0]-273],[0.],'o',color=color,ms=6,mfc='w')
+    ax.text(100,0.5,f"{temp[0]-273:.0f}"+'$^\circ{C}$',fontsize=12,ha='left',va='top',color=color)
+    ax.plot([temp[-1]-273],[Zbot*1e-3],'o',color=color,ms=4,mfc='w')
+    ax.text(900,Zbot*1e-3-0.3,f"{temp[-1]-273:.0f}"+'$^\circ{C}$',fontsize=12,ha='left',va='bottom',color=color)
 
-    ax.plot(tr-273, z/1e3,'k',label="Radiogenic")
-    tmoho = np.interp(Zmoho,z,tr)
-    ax.plot([tmoho-273],[Zmoho*1e-3],'o',color='k')
-    ax.text(tmoho-273-10,Zmoho*1e-3+1,f"{tmoho-273:.0f}",fontsize=12,ha='right',va='top')
-
-    ax.plot(tr3-273, z/1e3,'--b',label="Radiogenic 3 layers")
-    tmoho3 = np.interp(Zmoho,z,tr3)
-    ax.plot([tmoho3-273],[Zmoho*1e-3],'o',color='b')
-    ax.text(tmoho3-273+10,Zmoho*1e-3-1,f"{tmoho3-273:.0f}",fontsize=12,ha='left',va='bottom',color='b')
-    ax.plot([tr3[-1]-273],[Zbot*1e-3],'o',color='b')
-    ax.text(tr3[-1]-273-40,Zbot*1e-3-0.3,f"{tr3[-1]-273:.0f}",fontsize=12,ha='right',va='bottom',color='b')
-
-    ax.text(300,90,f'q={qm*1e3:.0f} mW/m$^2$',fontsize=12,ha='left',va='top')
-    ax.text(300,93,f'q={qm3*1e3:.0f} mW/m$^2$',fontsize=12,ha='left',va='top',color='b')
-
+    ax.text(300,93,f'q={qm*1e3:.0f} mW/m$^2$',fontsize=12,ha='left',va='top',color=color)
 
     ax2 = ax.twiny()
     
-    ax2.plot(hz*1e6,z/1e3,'--',color='k',label="Heat production",lw=1)
-    ax2.plot(hz3*1e6,z/1e3,'--',color='b',label="H only in crust",lw=1)
+    ax2.plot(hz3*1e6,z/1e3,'--',color='r',label="Heat production",lw=1)
     ax2.set_xlabel(r"Heat production ($\mu$W/m3)")
     ax2.legend(loc='upper right')
     ax2.set_xlim(0,4)
@@ -173,7 +149,8 @@ def main():
     ax.set_xlim(0,1500)
     ax.grid(ls='--')
     fig.tight_layout()
-    filename = f'geo-{nlayer:d}-{H0:e}-{hr/1e3:.0f}km.png'
+    filename = f'geo-{nlayer:d}-{rhoH0_arr[0]:.1e}-{hr/1e3:.0f}km.png'
+    # filename = 'test.png'
     
     print(f'save figure to {filename}')
     fig.savefig(filename)
