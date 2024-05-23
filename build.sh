01f1ef17e82a9a416f4306c80e91bed05c1d9ffc
Chase J. Shyu <iamhemry@gmail.com>
2024-05-23 23:03:44 +0800

    keep last two version of build image

--- build.sh
+++ build.sh
@@ -21,8 +21,14 @@ fi
 
 # Pull the base image
 docker pull $BASE_IMAGE
+
+NEW_TAG="dynearthsol/$CXXVERSION:new"
+LATEST_TAG="dynearthsol/$CXXVERSION:latest"
+OLD_TAG="dynearthsol/$CXXVERSION:old"
+LATEST_IMAGE_ID=$(docker images -q $LATEST_TAG)
+
 # Build the docker image
-docker build --rm -t dynearthsol/$CXXVERSION . \
+docker build --rm -t $NEW_TAG . \
     --build-arg CXXVERSION=$CXXVERSION \
     --build-arg BASE_IMAGE=$BASE_IMAGE \
     --build-arg NDIMS=$NDIMS \
@@ -31,11 +37,25 @@ docker build --rm -t dynearthsol/$CXXVERSION . \
 # Check if the build was successful
 if [ $? -eq 0 ]; then
     echo "Docker build succeeded."
+    NEW_IMAGE_ID=$(docker images -q $NEW_TAG)
+    if [ "$NEW_IMAGE_ID" != "$LATEST_IMAGE_ID" ]; then
+        echo "Image has changed. Updating tag."
+        docker rmi $OLD_TAG
+        docker tag $LATEST_TAG $OLD_TAG
+        docker tag $NEW_TAG $LATEST_TAG
+        docker rmi $NEW_TAG
+        docker image prune -f
+    else
+        echo "Image has not changed. Keeping old tag."
+        docker rmi $NEW_TAG
+    fi
     echo ""
     echo "To run the container execute:"
-    echo "$ docker run -it --rm dynearthsol/$CXXVERSION"
+    echo "$ docker run -it --rm $LATEST_TAG"
 else
     echo "Docker build failed. Cleaning up..."
-    docker rmi dynearthsol/$CXXVERSION
+    docker rmi $NEW_TAG
 fi
+# Remove dangling images
+docker image prune -f
 
